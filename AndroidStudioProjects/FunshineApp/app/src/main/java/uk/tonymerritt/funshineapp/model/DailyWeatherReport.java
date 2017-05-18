d5021c0a50ca5003cdb09b349fd83d69fa3488df
Tony Merritt <Tony@klavar.co.uk>
2017-05-18 23:41:00 +0100

    Initial commit

---
+++ AndroidStudioProjects/FunshineApp/app/src/main/java/uk/tonymerritt/funshineapp/model/DailyWeatherReport.java
@@ -0,0 +1,8 @@
+package uk.tonymerritt.funshineapp.model;
+
+/**
+ * Created by Klavar on 09/05/2017.
+ */
+
+public class DailyWeatherReport {
+}
---
+++ AndroidStudioProjects/KlavarChatAndroid/app/src/main/java/uk/tonymerritt/klavarchatandroid/activities/model/User.java
@@ -0,0 +1,8 @@
+package uk.tonymerritt.klavarchatandroid.activities.model;
+
+/**
+ * Created by Klavar on 18/05/2017.
+ */
+
+public class User {
+}
